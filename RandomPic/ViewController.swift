c0417f24bc85051518cfd8ba646d4c14ceea8270
Tony Chen <tony.chen@concentrix.com>
2022-10-25 17:21:21 +1100

    change back

--- RandomPic/ViewController.swift
+++ RandomPic/ViewController.swift
@@ -65,7 +65,7 @@ class ViewController: UIViewController {
             imageView.widthAnchor.constraint(equalToConstant: 300),
             imageView.heightAnchor.constraint(equalToConstant: 300),
 
-            button.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 26),
+            button.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 24),
             button.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 24),
             button.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -24),
             button.heightAnchor.constraint(equalToConstant: 40)
